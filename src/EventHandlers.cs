639f6738b1f6f4845b2f046794fc7cc870331679
TheKillerIdle <samupanizo@gmail.com>
2023-02-21 01:28:28 +0100

    Bum update

--- src/EventHandlers.cs
+++ src/EventHandlers.cs
@@ -1,6 +1,9 @@
 ﻿using System.Xml;
+using CommandSystem;
+using CommandSystem.Commands.RemoteAdmin;
 using Exiled.API.Features;
 using Exiled.API.Features.Items;
+using Exiled.API.Features.Pickups.Projectiles;
 using Exiled.Events;
 using Exiled.Events.EventArgs.Player;
 using Exiled.Events.Handlers;
@@ -8,8 +11,13 @@ using GameCore;
 using InventorySystem.Items.Firearms.Utilities;
 using InventorySystem.Items.ThrowableProjectiles;
 using MapGeneration.Distributors;
+using Mirror;
+using PluginAPI.Core.Items;
+using RemoteAdmin;
+using Respawning;
 using UnityEngine;
 using Cassie = Exiled.API.Features.Cassie;
+using Item = Exiled.API.Features.Items.Item;
 using Map = Exiled.API.Features.Map;
 using Player = Exiled.API.Features.Player;
 using Server = Exiled.API.Features.Server;
@@ -30,6 +38,25 @@ namespace PluginPrueba_EXILED
             {
                 ev.Attacker.Broadcast(Plugin.Singleton.Config.BroadcastDuration , Plugin.Singleton.Config.BroadcastMsg);
             }
+        }
+
+        public void OnWaitingPlayers()
+        {
+            Round.IsLocked = true;
+            Console.AddLog($"Roundlock activo", Color.blue);
+        }
+        
+
+        public void OnPlayerShooting(ShootingEventArgs ev)
+        {
+            var bum = (ExplosiveGrenade)Item.Create(ItemType.GrenadeHE);
+            bum.SpawnActive(ev.Player.Position);
+            Console.AddLog($"Granada spawned", Color.yellow);
+        }
+
+        public void OnPlayerJoined(JoinedEventArgs ev)
+        {
+            Cassie.Message("welcome killer",false, false, false);
         }
     }
 }
--- src/Plugin.cs
+++ src/Plugin.cs
@@ -6,7 +6,7 @@ namespace PluginPrueba_EXILED
     public class Plugin : Plugin<Config>
     {
         public override string Author { get; } = "TheKillerIdle";
-        public override string Name { get; } = "Broadcast tonto";
+        public override string Name { get; } = "Primerplugin";
         public override string Prefix { get; } = "plugin1";
         public override Version Version { get; } = new Version(1,1);
 
@@ -17,15 +17,20 @@ namespace PluginPrueba_EXILED
         {
             Singleton = this;
             EventHandlers = new EventHandlers();
+            Exiled.Events.Handlers.Server.WaitingForPlayers += EventHandlers.OnWaitingPlayers;
             Exiled.Events.Handlers.Server.RoundStarted += EventHandlers.OnRoundStarted;
             Exiled.Events.Handlers.Player.Dying += EventHandlers.OnPlayerDying;
+            Exiled.Events.Handlers.Player.Shooting += EventHandlers.OnPlayerShooting;
+            Exiled.Events.Handlers.Player.Joined += EventHandlers.OnPlayerJoined;
             base.OnEnabled();
         }
 
         public override void OnDisabled()
         {
+            Exiled.Events.Handlers.Server.WaitingForPlayers -= EventHandlers.OnWaitingPlayers;
             Exiled.Events.Handlers.Server.RoundStarted -= EventHandlers.OnRoundStarted;
             Exiled.Events.Handlers.Player.Dying -= EventHandlers.OnPlayerDying;
+            Exiled.Events.Handlers.Player.Shooting -= EventHandlers.OnPlayerShooting;
             EventHandlers = null;
             Singleton = null;
             base.OnDisabled();
